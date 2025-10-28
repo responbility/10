63053c09c78c43996360bc8bc8b9ca54570d8b27
duruwell <saomaster4@gmail.com>
2025-10-28 18:01:47 +0900

    ㅁㅇㄴ

--- boy.py
+++ boy.py
@@ -111,7 +111,7 @@ class Run:
 
 
 
-class boy:
+class Boy:
     def __init__(self):
         self.x, self.y = 400, 90
         self.frame = 0
@@ -122,20 +122,15 @@ class boy:
         self.IDLE = Idle(self)
         self.SLEEP = Sleep(self)
         self.RUN = Run(self)
-        self.state_machine = StateMachine(
-            self.IDLE,
-            {
+        self.state_machine = StateMachine()
 
-                self.SLEEP: {space_down: self.IDLE},
-                self.IDLE: {space_down: self.IDLE,time_out: self.SLEEP,right_down: self.RUN,left_down: self.RUN,right_up: self.RUN,left_up: self.RUN
-                },
-                self.RUN: {space_down: self.RUN,right_up: self.IDLE,left_up: self.IDLE,right_down: self.IDLE,left_down: self.IDLE
-                }
-            }
+
+             {
                 self.SLEEP : {space_down: self.IDLE},
-                self.IDLE : {space_down:self.IDL,Etime_out: self.SLEEP, right_down: self.RUN, left_down: self.RUN, right_up: self.RUN, left_up: self.RUN},
-                self.RUN : {space_down:self.RUN,right_up: self.IDLE, left_up: self.IDLE, right_down: self.IDLE, left_down: self.IDLE}
-            } #pace_down: self.IDLE 부분 수정
+                self.IDLE : {space_down: self.IDLE, time_out: self.SLEEP, right_down: self.RUN, left_down: self.RUN, right_up: self.RUN, left_up: self.RUN},
+                self.RUN : {space_down: self.RUN, right_up: self.IDLE, left_up: self.IDLE, right_down: self.IDLE, left_down: self.IDLE}
+            }
+
 
     def update(self):
         self.state_machine.update()
